2e704e96ab21859d5b1ad2790a92225f245c9584
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-12-01 23:03:02 -0300

    Chaning package

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,19 @@
-import { createConnection } from "typeorm";
+import { ConnectionOptions, createConnection } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config();
+const connectioOptions = {
+  type: process.env.DBTYPE,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.PASSWORD_DB,
+  host: process.env.DB_HOST,
+  migrations: [__dirname + "/src/database/migrations/*{.ts,.js}"],
+  entities: [__dirname + "/src/models/*{.ts,.js}"],
+  cli: {
+    migrationsDir: "./src/database/migrations",
+  },
+  cache: false,
+  synchronize: true,
+} as ConnectionOptions;
 
-createConnection();
+createConnection(connectioOptions);
