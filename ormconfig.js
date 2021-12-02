d5c65858c4607a92976cbf372b566b2998506115
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-12-01 22:49:49 -0300

    Chaning package

--- ormconfig.js
+++ ormconfig.js
@@ -4,8 +4,8 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.PASSWORD_DB,
   host: process.env.DB_HOST,
-  migrations: [__dirname + "/src/database/migrations/*.ts"],
-  entities: [__dirname + "/src/models/*.ts"],
+  migrations: [__dirname + "/src/database/migrations/*{.ts,.js}"],
+  entities: [__dirname + "/src/models/*{.ts,.js}"],
   cli: {
     migrationsDir: "./src/database/migrations",
   },
